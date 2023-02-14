f74ddedd33c38e6777ff8b0a713d529d574b4d26
Rupendra0026 <rupendravnvss@gmail.com>
2023-02-15 00:09:37 +0530

    changes

--- index.js
+++ index.js
@@ -7,7 +7,7 @@ const cors=require('cors');
 app.use(cors());
 app.use(express.json());
 const bodyparser=require("body-parser");
-const PORT=5000||process.env.PORT;
+const PORT=process.env.PORT||5000;
 const mongoose=require("mongoose");
 mongoose.connect(process.env.DATABASE,({ useNewUrlParser: true, useUnifiedTopology: true })).then(()=>{
     console.log("connected to database");
